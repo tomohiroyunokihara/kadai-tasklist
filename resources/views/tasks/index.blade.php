857f9fe3162058ae3c06dd8399af64b5389576e1
EC2 Default User <ec2-user@ip-172-31-2-171.us-east-2.compute.internal>
2018-06-04 01:27:56 +0000

    shusei5

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -28,7 +28,7 @@
             </tbody>
         </table>
     @endif
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
     </div>
     </div>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -13,16 +13,16 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>タイトル</th>
-            <td>{{ $task->title }}</td>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
     
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
