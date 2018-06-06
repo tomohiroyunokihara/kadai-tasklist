61d435abd5757421da4e4f2d2996fd1a314caba8
EC2 Default User <ec2-user@ip-172-31-2-171.us-east-2.compute.internal>
2018-06-06 02:02:12 +0000

    login function

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,17 +8,22 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Microposts</a>
+                @if (Auth::check())
+                <a class="navbar-brand" href="/tasks">Tasklist</a>
+                @else
+                <a class="navbar-brand" href="/">Tasklist</a>
+                @endif
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
-                        <li><a href="#">Tasks</a></li>
+                        <li><a href='/tasks/create?'>新規タスクの投稿</a></li>
+                        <!--<li>{!! link_to_route('signup.get', '新規タスクの投稿') !!}</li>-->
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">My profile</a></li>
-                                <li role="separator" class="divider"></li>
+
+
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
                         </li>
